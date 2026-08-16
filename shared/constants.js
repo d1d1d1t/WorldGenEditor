555bb8e8be77bd7573e47d1ce77e379867e5f828
d1d1d1t <designsbyd1nero@gmail.com>
2026-08-15 20:09:31 -0700

    Update constants.js

--- shared/constants.js
+++ shared/constants.js
@@ -1,20 +1,46 @@
+/*
+ * shared/constants.js
+ *
+ * Convenience accessors for canonical project geometry.
+ */
+
 import { TILE_SPEC } from "../data/tile-spec.js";
 
-export const CONSTANTS = {
-  // micro tiles
-  mTile: TILE_SPEC.micro.mTile,
-  rTile: TILE_SPEC.micro.rTile,
-  eTile: TILE_SPEC.micro.eTile,
+export const CONSTANTS = Object.freeze({
 
-  // macro tiles
-  tTile: TILE_SPEC.macro.terrain.size,
-  pTile: TILE_SPEC.macro.road.size,
-  fTile: TILE_SPEC.macro.feature.size,
+    world: Object.freeze({
+        width: TILE_SPEC.world.width,
+        height: TILE_SPEC.world.height
+    }),
 
-  // canvas bounds
-  canvas: {
-    terrain: TILE_SPEC.widgets.terrain,
-    road: TILE_SPEC.widgets.road,
-    feature: TILE_SPEC.widgets.feature
-  }
-};
+    terrain: Object.freeze({
+
+        tTile: TILE_SPEC.terrain.macro,
+        mTile: TILE_SPEC.terrain.micro,
+
+        grid: TILE_SPEC.terrain.grid
+
+    }),
+
+    road: Object.freeze({
+
+        pTile: TILE_SPEC.road.macro,
+        rTile: TILE_SPEC.road.micro
+
+    }),
+
+    feature: Object.freeze({
+
+        fTile: TILE_SPEC.feature.macro,
+        eTile: TILE_SPEC.feature.micro
+
+    }),
+
+    overpass: Object.freeze({
+
+        width: TILE_SPEC.overpass.width,
+        height: TILE_SPEC.overpass.height
+
+    })
+
+});
