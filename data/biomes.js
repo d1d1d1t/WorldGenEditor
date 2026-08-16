e4920bc964d7566f720fe3141c29073c07e3962d
d1d1d1t <designsbyd1nero@gmail.com>
2026-08-15 20:08:55 -0700

    Update biomes.js

--- data/biomes.js
+++ data/biomes.js
@@ -7,22 +7,27 @@
 export const BIOMES = Object.freeze({
 
     forest: Object.freeze({
+        id: "forest",
         name: "Forest"
     }),
 
     plains: Object.freeze({
+        id: "plains",
         name: "Plains"
     }),
 
     tundra: Object.freeze({
+        id: "tundra",
         name: "Tundra"
     }),
 
     wasteland: Object.freeze({
+        id: "wasteland",
         name: "Wasteland"
     }),
 
     molten: Object.freeze({
+        id: "molten",
         name: "Molten"
     })
 
